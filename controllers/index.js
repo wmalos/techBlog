005cfbd49affc10ea6b72bc9eb571bc8a340ae72
Will <wmalos22@gmail.com>
2021-05-18 14:27:19 -0700

    adding rout files and updating index

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes.js');
+const dashRoutes = require('./dashboardRoutes.js');
 
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashRoutes);
 router.use((req, res) => {
     res.status(404).end();
 });
