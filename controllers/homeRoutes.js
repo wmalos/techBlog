85eed275afab50356387c984bafcef697745e280
Will <wmalos22@gmail.com>
2021-05-20 16:18:27 -0700

    fixed post

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -65,7 +65,7 @@ router.get("/post/:id", async (req, res) => {
       ],
     });
 
-    if (!onePost) {
+    if (!findPost) {
       res.status(404).json({ message: "This post does not exist" });
       return;
     }
@@ -81,27 +81,3 @@ router.get("/post/:id", async (req, res) => {
 
 
 
-
-
-
-
-
-// Use withAuth middleware to prevent access to route
-router.get("/profile", withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ["password"] },
-      include: [{ model: Post }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render("profile", {
-      ...user,
-      logged_in: true,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
