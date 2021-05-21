991b5dff59a221cfab8499d332c5708ed116a593
Will <wmalos22@gmail.com>
2021-05-20 18:49:05 -0700

    fixed post routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const postRoutes = require('./postRroutes');
+const postRoutes = require('./postRoutes');
 const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
